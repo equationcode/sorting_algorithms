9b67e42107774d01a318fb7ab2dd0cce805ac094
equationcode <annypaul80@gmail.com>
2023-07-31 04:47:24 -0700

    101-cocktail_sort_list.c

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap_node - function that swap a node for his previous one
+ * @node: the node
+ * @list: the list of the node
+ * Return: pointer to the node
+ */
+
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
+}
+
+/**
+ * cocktail_sort_list - function that sorts a doubly linked list of
+ * integers in ascending order using the cocktail shaker sort
+ * @list: the list
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+	listint_t *node;
+	int swap_done = 1;
+
+	if (list == '\0' || (*list) == '\0' || (*list)->next == '\0')
+		return;
+	node = *list;
+	while (swap_done == 1)
+	{
+		swap_done = 0;
+		while (node->next)
+		{
+			if (node->n > node->next->n)
+			{
+				node = swap_node(node->next, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			node = node->next;
+		}
+		if (swap_done == 0)
+			break;
+		swap_done = 0;
+		while (node->prev)
+		{
+			if (node->n < node->prev->n)
+			{
+				node = swap_node(node, list);
+				swap_done = 1;
+				print_list(*list);
+			}
+			else
+				node = node->prev;
+		}
+	}
+}
